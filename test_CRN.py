f97f72e50b25928e4a70aba522b196cf4a79d87f
shangwei5 <43960503+shangwei5@users.noreply.github.com>
2019-11-05 20:03:37 +0800

    Update test_CRN.py

--- test_CRN.py
+++ test_CRN.py
@@ -15,14 +15,11 @@ import time
 #os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 
 
-parser = argparse.ArgumentParser(description="DnCNN_Test")
-parser.add_argument("--num_of_layers", type=int, default=17, help="Number of total layers")
+parser = argparse.ArgumentParser(description="DCRN_Test")
 parser.add_argument("--logdir", type=str, default="logs/CRN/R100H", help='path of log files')
-parser.add_argument("--test_data", type=str, default='Set12', help='test on Set12 or Set68')
-parser.add_argument("--test_noiseL", type=float, default=25, help='noise level used on test set')
-parser.add_argument("--data_path", type=str, default="/home/shangwei/dataset/R100H/test", help='path to training data')
+parser.add_argument("--data_path", type=str, default="dataset/...", help='path to testing data')
 parser.add_argument("--save_path", type=str, default="result/CRN/R100H/output", help='path to save results')
-parser.add_argument("--save_path_r", type=str, default="result/CRN/R100H/rainstreak", help='path to save rainresults')
+parser.add_argument("--save_path_r", type=str, default="result/CRN/R100H/rainstreak", help='path to save rain streaks')
 parser.add_argument("--use_GPU", type=bool, default=True, help='use GPU or not')
 parser.add_argument("--gpu_id", type=str, default="0", help='GPU id')
 parser.add_argument("--inter_iter", type=int, default=4, help='number of inter_iteration')
